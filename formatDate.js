070b4487322563535195a9e0bbcb0d79b8bbfc0f
Kuohao-wu <www.guohao5789@gmail.com>
2017-10-26 01:30:59 -0500

    添加日期本地化功能

--- formatDate.js
+++ formatDate.js
@@ -1,37 +1,60 @@
-function formatDate(formatStr,date) {
-  // 此转换器的核心是，将时间对象格式化为yyyy-MM-dd hh:mm:ss这样的形式
-  // 使用正则表达式进行匹配替换
-
+@formatStr String 'yy-MM-dd hh:mm:ss'
+@params Object {date: Date, localize: Boolean}
+function formatDate(formatStr, opt) {
+  let date = opt.date
   // 初始化一个最后返回的变量
-  let str = ''
+  let str = formatStr 
+  let localize = opt.localize
   // 因为年份可能需要截取，所以单独拎出来替换，根据格式化字符串的长度来确定到底需要留几个字符串
-  if(/(y+)/.test(formatStr)){
-    let veryYear = date.getFullYear().toString()
-    str = formatStr.replace(RegExp.$1, veryYear.substr(veryYear.length - RegExp.$1.length))
+  if (/(y+)/.test(formatStr)) {
+    if(localize) {
+      let veryYear = date.getFullYear().toString()
+    	str = str.replace(RegExp.$1 + '-', veryYear.substr(veryYear.length - RegExp.$1.length) + '年')
+    }else {
+      let veryYear = date.getFullYear().toString()
+    	str = str.replace(RegExp.$1, veryYear.substr(veryYear.length - RegExp.$1.length))
+    }
   }
-
-  let o = {
-    'M+' : date.getMonth() + 1,
-    'd+' : date.getDate(),
-    'h+' : date.getHours(),
-    'm+' : date.getMinutes(),
-    's+' : date.getSeconds()
+	
+  
+  const createPattern = (isLocalize, date) => {
+    let o = {}
+    if(isLocalize) {
+        o = {
+        'M+-': date.getMonth() + 1 + '月',
+        'd+\s?': date.getDate() + '日',
+        'h+:': date.getHours() + '时',
+        'm+:': date.getMinutes() + '分',
+        's+': date.getSeconds() + '秒'
+      }
+    } else {
+      o = {
+        'M+': date.getMonth() + 1,
+        'd+': date.getDate(),
+        'h+': date.getHours(),
+        'm+': date.getMinutes(),
+        's+': date.getSeconds()
+      }
+    }
+    return o
   }
-
+  
+  let pattern = createPattern(true, date)
+  
   // 循环匹配，直到匹配完所有格式化字符串为止
-  for(i in o) {
+  for (let i in pattern) {
     // 正则表达式中RegExp.$1要先进行test，exec等匹配方法之后才能获取
-    if(new RegExp(`(${i})`,'g').test(formatStr)){
+    if (new RegExp(`(${i})`, 'g').test(formatStr)) {
       // 因为单月前面一般都要补0，所以得要对o[i] 进行一些处理,如MM则补0
-      str = str.replace(RegExp.$1,RegExp.$1.length > 1 ? addZreo(o[i]) : o[i])
+      str = str.replace(RegExp.$1, RegExp.$1.length > 1 ? addZreo(pattern[i]) : pattern[i])
     }
   }
   return str
 
   // 补0函数，需要保留多少位，就加多少个0在前面
-  function addZreo(num){
-    let str = num.toString()
-    return ('00' + str).substr(str.length)
+  function addZreo(num) {
+    let getStr = num.toString().replace(/\d+/, '')
+    let str = parseInt(num).toString()
+    return ('00' + str).substr(str.length) + getStr
   }
-  
 }
